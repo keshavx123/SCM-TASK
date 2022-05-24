b3010299474f78f011aedfb4f3db37dc381413fc
Karamjeet Singh <93766519+ks9211@users.noreply.github.com>
2022-05-24 23:08:13 +0530

    Update Patterns.cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -12,3 +12,14 @@ int main()
     {
         for(int space = 0; space < rows-i; ++space)
             cout << "  ";
+     for(int j = i; j <= 2*i-1; ++j)
+            cout << "* ";
+
+        for(int j = 0; j < i-1; ++j)
+            cout << "* ";
+
+        cout << endl;
+    }
+
+    return 0;
+}
